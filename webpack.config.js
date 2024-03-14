d1d59b39dcd27cde850c3976b11b61c5efc5b78c
yuliabgkv <bugakovayk@gmail.com>
2024-03-14 13:54:40 +0200

    initial commit (basic project settings)

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+
+
+module.exports = {
+    mode,
+    target,
+    devtool: 'source-map',
+    devServer: { open: true },
+    entry: ["@babel/polyfill", path.resolve(__dirname, 'src', 'index.js')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')]
+                            }
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            }
+        ]
+    }
+}
